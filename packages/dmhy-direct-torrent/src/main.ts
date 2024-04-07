f4806fa36befe3a0af356ad74a80bbd6c520be61
CYTMWIA <58131156+CYTMWIA@users.noreply.github.com>
2024-04-07 18:08:56 +0800

    在原页面打开下载链接；避免重入按钮处理函数；

--- packages/dmhy-direct-torrent/src/main.ts
+++ packages/dmhy-direct-torrent/src/main.ts
@@ -1,38 +1,42 @@
 import './style.css';
 
 function open_in_new_tab(url: string) {
-  let handle = window.open(url, '_blank')
-  // Open new window without focus on it
-  // https://stackoverflow.com/questions/15133605/open-new-window-without-focus-on-it)
-  handle?.blur()
-  window.focus()
+  let v = document.createElement("a")
+  v.setAttribute("href", url)
+  v.click()
 }
 
-function download_torrent(item_url: string) {
-  fetch(item_url).then((resp) => {
-    return resp.text()
-  }).then((html) => {
-    let find_url = html.match(/\/\/.*?\.torrent/)
-    if (!find_url) {
-      alert("没有找到种子链接")
-      return
-    }
-    let url = find_url[0]
-    open_in_new_tab(url)
-  }).catch((reason) => {
+async function __download_torrent(item_url: string) {
+  let resp = await fetch(item_url)
+  let html = await resp.text()
+  let find_url = html.match(/\/\/.*?\.torrent/)
+  if (!find_url) {
+    alert("没有找到种子链接")
+    return
+  }
+  let url = find_url[0]
+  open_in_new_tab(url)
+}
+
+async function download_torrent(item_url: string) {
+  try {
+    await __download_torrent(item_url)
+  } catch (error) {
     let msg = "获取资源页面失败"
     alert(msg)
-    console.log(msg, reason)
-  })
+    console.log(msg, error)
+  }
 }
 
 function create_button(item_url: string) {
-  let btn = document.createElement("a")
-  btn.innerText = "[种子]"
+  let btn = document.createElement("button")
+  btn.innerText = "种子"
   btn.style.cursor = "pointer"
-  btn.onclick = () => {
+  btn.onclick = async () => {
+    btn.disabled = true
     console.log(item_url)
-    download_torrent(item_url)
+    await download_torrent(item_url)
+    btn.disabled = false
   }
   return btn
 }
