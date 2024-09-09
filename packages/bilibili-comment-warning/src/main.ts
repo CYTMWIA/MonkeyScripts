581e07ec43b6653b7ff148414f0d16be50d4756b
CYTMWIA <58131156+CYTMWIA@users.noreply.github.com>
2024-09-09 20:34:55 +0800

    B 站评论区 dom 更新

--- packages/bilibili-comment-warning/src/main.ts
+++ packages/bilibili-comment-warning/src/main.ts
@@ -2,13 +2,22 @@ import './style.css'
 
 let WARNING_TEXT = "/// 警告！您已进入评论区，请远离无谓的网络争吵 ///"
 
+function find_comment_container() {
+  let v20240909 = document.getElementById("commentapp")
+  if (v20240909) return v20240909
+
+  let old_comment_container = document.getElementsByClassName("comment")
+  if (old_comment_container.length) return old_comment_container[0]
+
+  return null
+}
+
 setInterval(() => {
-  let maybe_comment_container = document.getElementsByClassName("comment")
-  if (maybe_comment_container.length === 0) {
+  let comment_container = find_comment_container()
+  if (comment_container === null) {
     console.log("获取评论区位置失败")
     return
   }
-  let comment_container = maybe_comment_container[0]
 
   let ws = document.getElementsByClassName("comment-warning-container")
   if (ws.length === 0) {
